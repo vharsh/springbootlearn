cdeeaa40ed04daf510ab9d4f108ce1064e09a558
Harsh Vardhan <thevharsh@gmail.com>
2023-08-29 19:07:04 +0530

    init: add initialized maven springboot3 web project

---
+++ helloworld/src/test/java/sh/har/springboot/demo/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.har.springboot.demo.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
